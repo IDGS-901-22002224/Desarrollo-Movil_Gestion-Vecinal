c698cbb9b7dbd8a66d938e8dee5bd65a28be28aa
IDGS-801-22000823 <gutierrez.ascencio.luis@gmail.com>
2025-11-06 13:39:00 -0600

    Implementa modulo de servicios

---
+++ app/src/main/java/org/utl/reddeseguridadvecinal/Historial_servicios.kt
@@ -0,0 +1,20 @@
+package org.utl.reddeseguridadvecinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Historial_servicios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_historial_servicios)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
