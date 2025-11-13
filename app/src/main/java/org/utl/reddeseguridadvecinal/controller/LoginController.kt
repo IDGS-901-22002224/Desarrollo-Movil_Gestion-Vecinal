c753f3efc607a8107c6d9adce8791b27476045ad
IDGS-901-22000823 <gutierrez.ascencio.luis@gmail.com>
2025-11-12 23:41:44 -0600

    Validar que no sean guardias

--- app/src/main/java/org/utl/reddeseguridadvecinal/controller/LoginController.kt
+++ app/src/main/java/org/utl/reddeseguridadvecinal/controller/LoginController.kt
@@ -6,14 +6,12 @@ import org.utl.reddeseguridadvecinal.modelo.LoginResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-
 // Interface para la API
 interface LoginApiService {
     @POST("api/Usuarios/login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
 }
 
-// Controller
 class LoginController {
     private val apiService = Api.createService(LoginApiService::class.java)
 
@@ -23,6 +21,12 @@ class LoginController {
 
             val loginRequest = LoginRequest(email, password)
             val response = apiService.login(loginRequest)
+
+            //VALIDAR QUE NO SEA GUARDIA
+            if (response.tipoUsuario.equals("Guardia", ignoreCase = true)) {
+                //println("Acceso denegado a guardia")
+                return null
+            }
 
             //println("Login exitoso")
             response
