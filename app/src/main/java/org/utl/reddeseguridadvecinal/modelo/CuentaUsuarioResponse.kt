3f73fa28a9501c459884cfaea34f41c993b0889d
IDGS-801-22000823 <gutierrez.ascencio.luis@gmail.com>
2025-11-26 22:30:32 -0600

    Cambios de configuraciones

--- app/src/main/java/org/utl/reddeseguridadvecinal/modelo/CuentaUsuarioResponse.kt
+++ app/src/main/java/org/utl/reddeseguridadvecinal/modelo/CuentaUsuarioResponse.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.utl.reddeseguridadvecinal.modelo
 
 data class CuentaUsuarioResponse(
@@ -9,16 +8,4 @@ data class CuentaUsuarioResponse(
     val ultimaActualizacion: String,
     val cargosMantenimiento: List<CargoMantenimientoDTO>,
     val cargosServicios: List<CargoServicioDTO>
-=======
-package org.utl.reddeseguridadvecinal.modelo
-
-data class CuentaUsuarioResponse(
-    val cuentaID: Int,
-    val saldoMantenimiento: Double,
-    val saldoServicios: Double,
-    val saldoTotal: Double,
-    val ultimaActualizacion: String,
-    val cargosMantenimiento: List<CargoMantenimientoDTO>,
-    val cargosServicios: List<CargoServicioDTO>
->>>>>>> 78d914d85a3eb3b4bfa69f32dfd5a85ccf02eb3a
 )
